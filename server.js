406e3ce3f2521d6d0dd08eecc89525a4d71f5094
moeabraham <moeabraham12@gmail.com>
2022-05-23 22:30:03 -0500

    init repo

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db')
+
 
 const app = express();
+
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }))
+
 
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the Contact Keeper API'}))
 
@@ -12,6 +20,6 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
 
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=> console.log(`Server started on ${PORT}`))
